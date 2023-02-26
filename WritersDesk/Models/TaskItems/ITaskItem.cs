3351b4639019608bab58e23a19ed10db29cc6285
Glenn Abram Newcomer <gnewcomera@gmail.com>
2023-02-26 15:26:51 -0500

    Remove ignored files

---
+++ WritersDesk/Models/TaskItems/ITaskItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WritersDesk.Models.TaskItems
+{
+    public interface ITaskItem
+    {
+        /// <summary>
+        /// The name of this task
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// The description of this task
+        /// </summary>
+        string Description { get; set; }
+
+        /// <summary>
+        /// Whether this task is complete or not
+        /// </summary>
+        bool IsComplete { get; set; }
+
+    }
+}
